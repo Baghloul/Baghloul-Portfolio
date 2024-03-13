c1081d8eb485ab6f3c5bd9bcf4822cba5e6feaa3
Baghloul <163215722+Baghloul@users.noreply.github.com>
2024-03-13 08:59:30 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,18 +162,7 @@
   /**
    * Intro type effect
    */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  
 
   /**
    * Initiate portfolio lightbox 
